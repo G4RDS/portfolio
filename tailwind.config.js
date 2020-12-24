759ed338ddaf00b6bbc42f5fba4d61f416dfa088
G4RDS <account@g4rds.jp>
2020-12-24 15:36:53 +0900

    Configure Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    boxShadow: {
+      DEFAULT: '0 1px 3px rgb(51, 65, 85, 0.05)',
+      DEFAULT: '0 4px 6px -1px rgb(51, 65, 85, 0.05)',
+      lg: '0 8px 16px rgb(51, 65, 85, 0.05)',
+    },
+    extend: {
+      colors: {
+        gray: colors.blueGray,
+      },
+    },
   },
   variants: {
     extend: {},
