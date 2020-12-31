2fc600e93b57f38f67b1b7d8dc88105a1f53153e
G4RDS <account@g4rds.jp>
2020-12-31 16:16:53 +0900

    Change navigation width

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,7 +4,7 @@ import { faCopyright } from '@fortawesome/pro-regular-svg-icons';
 
 export const Navigation: React.FC = ({ children }) => {
   return (
-    <div className="fixed inset-y-8 inset-l-8 flex flex-col w-80 py-8 rounded-3xl bg-white shadow-lg">
+    <div className="fixed inset-y-8 inset-l-8 flex flex-col w-72 py-8 rounded-3xl bg-white shadow-lg">
       <ul className="flex-grow">{children}</ul>
       <div className="pl-8 mt-6 text-base text-gray-600">
         <FontAwesomeIcon icon={faCopyright} />
