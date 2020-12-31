e83423cd2ddfca0a2fd0642ba0585072466bc288
G4RDS <account@g4rds.jp>
2020-12-31 16:57:11 +0900

    Add works data

---
+++ src/data/work.ts
@@ -0,0 +1,43 @@
+import { Work } from 'consts/work';
+
+export const works: Work[] = [
+  {
+    id: '1',
+    imageUrl: '/example-work.png',
+    imageAlt: 'Wristyleのロゴ',
+    name: 'Wristyle',
+    description:
+      'スマートウォッチ専門の Web メディア。Web サイトと管理 Web アプリを開発。',
+    tags: ['Next.js'],
+  },
+  {
+    id: '2',
+    imageUrl: '/example-work.png',
+    imageAlt: 'Wristyleのロゴ',
+    name: 'Wristyle',
+    description:
+      'スマートウォッチ専門の Web メディア。Web サイトと管理 Web アプリを開発。',
+    tags: ['Next.js'],
+  },
+];
+
+export const pickUpWorks: Work[] = [
+  {
+    id: '1',
+    imageUrl: '/example-work.png',
+    imageAlt: 'Wristyleのロゴ',
+    name: 'Wristyle',
+    description:
+      'スマートウォッチ専門の Web メディア。Web サイトと管理 Web アプリを開発。',
+    tags: ['Next.js'],
+  },
+  {
+    id: '2',
+    imageUrl: '/example-work.png',
+    imageAlt: 'Wristyleのロゴ',
+    name: 'Wristyle',
+    description:
+      'スマートウォッチ専門の Web メディア。Web サイトと管理 Web アプリを開発。',
+    tags: ['Next.js'],
+  },
+];
