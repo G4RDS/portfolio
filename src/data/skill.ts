1ddf6b1d21072c0eca393806d1f39469267ecfb1
G4RDS <account@g4rds.jp>
2021-01-04 17:56:32 +0900

    Add skill interface and data

---
+++ src/data/skill.ts
@@ -0,0 +1,14 @@
+import { Skill } from 'consts/skill';
+
+export const skills: Skill[] = [
+  {
+    id: '1',
+    imageUrl: '',
+    name: 'React',
+  },
+  {
+    id: '2',
+    imageUrl: '',
+    name: 'Next.js',
+  },
+];
