7d95325b8ca0d2bd48ed73e9d5ac44e4142b525b
G4RDS <account@g4rds.jp>
2021-01-04 18:04:44 +0900

    Fix page background issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const Layout: React.FC = ({ children }) => {
-  return <div className="h-full p-8 bg-gray-50 ">{children}</div>;
+  return <div className="min-h-full p-8 bg-gray-50 ">{children}</div>;
 };
