2a2a90c44fef037ffb422a1bafb513da355d8c11
G4RDS <account@g4rds.jp>
2021-01-10 16:27:11 +0900

    Apply xstyled to Navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,15 +1,43 @@
 import React from 'react';
+import styled, { x } from '@xstyled/styled-components';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopyright } from '@fortawesome/pro-regular-svg-icons';
 
 export const Navigation: React.FC = ({ children }) => {
   return (
-    <div className="fixed inset-y-8 inset-l-8 flex flex-col w-72 py-8 rounded-3xl bg-white shadow-lg">
-      <ul className="flex-grow">{children}</ul>
-      <div className="pl-8 mt-6 text-base text-gray-500">
+    <Container>
+      <x.ul flex="1 1 0">{children}</x.ul>
+      <CopyRightWrapper>
         <FontAwesomeIcon icon={faCopyright} />
-        <span className="ml-2 text-base font-medium">Yuki SENSUI, G4RDS</span>
-      </div>
-    </div>
+        <x.span ml={2}>Yuki SENSUI, G4RDS</x.span>
+      </CopyRightWrapper>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  position: fixed;
+  top: 2rem;
+  bottom: 2rem;
+  left: 2rem;
+
+  display: flex;
+  flex-direction: column;
+
+  width: 72;
+  padding: 8 0;
+  border-radius: 3xl;
+  background: white;
+  box-shadow: lg;
+`;
+
+const CopyRightWrapper = styled.div`
+  margin-top: 6;
+  padding-left: 8;
+
+  color: gray-500;
+  font-size: base;
+  line-height: normal;
+  font-weight: medium;
+`;
