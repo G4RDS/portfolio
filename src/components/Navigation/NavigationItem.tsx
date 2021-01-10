2af1676bcbfa49132f51a68c2a30cc844a273abc
G4RDS <account@g4rds.jp>
2021-01-10 16:12:39 +0900

    Apply xstyled to NavigationItem

--- src/components/Navigation/NavigationItem.tsx
+++ src/components/Navigation/NavigationItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled, { css, th } from '@xstyled/styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { AllowedIcons, getIcon } from './module';
@@ -13,23 +14,55 @@ interface Props {
 export const NavigationItem: React.FC<Props> = ({
   name,
   icon,
-  isActive,
+  isActive = false,
   href,
 }) => {
-  const borderColor = isActive ? 'border-blue-500' : 'border-white';
-  const fontColor = isActive ? 'text-blue-600' : 'text-gray-600';
-
   return (
-    <a
-      className={`flex items-center w-full mt-6 py-2 pl-8 border-r-4 ${borderColor} transition-all first:mt-0 hover:border-blue-500`}
-      href={href}
-    >
-      <FontAwesomeIcon
-        className={`text-2xl ${fontColor}`}
-        icon={getIcon(icon)}
-        fixedWidth
-      />
-      <span className={`ml-3 text-xl font-medium ${fontColor}`}>{name}</span>
-    </a>
+    <StyledAnchor href={href} isActive={isActive}>
+      <IconWrapper isActive={isActive}>
+        <FontAwesomeIcon icon={getIcon(icon)} fixedWidth />
+      </IconWrapper>
+      <NavName isActive={isActive}>{name}</NavName>
+    </StyledAnchor>
   );
 };
+
+const StyledAnchor = styled.a<{ isActive: boolean }>`
+  ${(p) => css`
+    display: flex;
+    align-items: center;
+
+    width: full;
+    margin: 6 0;
+    padding: 2 0 2 8;
+    border-right-width: 4;
+    border-right-style: solid;
+    border-color: ${p.isActive ? th.color('primary-500') : 'white'};
+
+    transition: all;
+
+    &:first-child {
+      margin-top: 0;
+    }
+
+    &:hover {
+      border-color: ${th.color('primary-500')};
+    }
+  `}
+`;
+
+const IconWrapper = styled.div<{ isActive: boolean }>`
+  ${(p) => css`
+    font-size: 2xl;
+    color: ${p.isActive ? th.color('primary-600') : th.color('gray-600')};
+  `}
+`;
+
+const NavName = styled.span<{ isActive: boolean }>`
+  ${(p) => css`
+    margin-left: 3;
+    font-size: xl;
+    font-weight: medium;
+    color: ${p.isActive ? th.color('primary-600') : th.color('gray-600')};
+  `}
+`;
